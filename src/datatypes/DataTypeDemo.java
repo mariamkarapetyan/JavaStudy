5f397605f8133c683e7a4847308a145c6f48094e
Karen Vardanyan <karenvardanyan@workfront.com>
2018-01-20 15:06:47 +0400

    java data type not complate version.

--- src/datatypes/DataTypeDemo.java
+++ src/datatypes/DataTypeDemo.java
@@ -3,6 +3,10 @@ package datatypes;
 public class DataTypeDemo {
 
     public static void byteDemo() {
+        byte  b = 0x10;
+        byte  a = 1;
+        byte  c = (byte) (b + a);
+
 
     }
 
@@ -12,6 +16,11 @@ public class DataTypeDemo {
 
     public static void charDemo() {
 
+        char ch = '\u0000';
+        char ch1 = '8';
+        byte i = 1;
+        ch1 = (char) i;
+
     }
 
     public static void intDemo() {
@@ -20,9 +29,16 @@ public class DataTypeDemo {
 
     public static void longDemo() {
 
+        int i = 7;
+        long l = 9;
+        long l1 = i + l;
+        int i1 = (int) (i + l);
     }
 
     public static void floatDemo() {
+
+        float f = 0.2f;
+        long l = 3;
 
     }
 
@@ -32,6 +48,12 @@ public class DataTypeDemo {
 
     public static void booleanDemo() {
 
+        boolean boll = true; // false
+        byte  b = 0x10;
+        byte  a = 1;
+//        if(a = b){
+//
+//        }
     }
 
 }
