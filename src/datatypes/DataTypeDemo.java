d8e3f108f4339f7d9fa557524128639bf099e532
Karen Vardanyan <karenvardanyan@workfront.com>
2018-01-20 13:39:43 +0400

    Initial commit for java data type demo.

---
+++ src/datatypes/DataTypeDemo.java
@@ -0,0 +1,37 @@
+package datatypes;
+
+public class DataTypeDemo {
+
+    public static void byteDemo() {
+
+    }
+
+    public static void shortDemo() {
+
+    }
+
+    public static void charDemo() {
+
+    }
+
+    public static void intDemo() {
+
+    }
+
+    public static void longDemo() {
+
+    }
+
+    public static void floatDemo() {
+
+    }
+
+    public static void doubleDemo() {
+
+    }
+
+    public static void booleanDemo() {
+
+    }
+
+}
